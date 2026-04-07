45e8cf5106b49c22767d14302f718c5a0001961e
say-netizen <say66yeah@gmail.com>
2026-04-07 19:29:45 +0900

    Update layout.tsx

--- contractguard/src/app/layout.tsx
+++ contractguard/src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="7b69bbd8b09c39ee" />
+        <meta name="google-site-verification" content="UnrUm_vb50Qz0N4ne5zpDEzY35hGdNZFQP_76NbTlJc" />
       </head>
       <body>{children}</body>
     </html>
